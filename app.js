c82ea1bd33475f90615f8e772d3b2843dcdad1f4
Daniel Fordham <dfordham46@gmail.com>
2024-05-04 13:03:38 -0400

    barest bones established

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-alert("CHAOS REIGNS")
+//order is yellow - blue / red - green
+document.querySelector(".js-yellow-button").onclick = function () {
+    alert("yellow.");
+}
+document.querySelector(".js-blue-button").onclick = function () {
+    alert("blue.");
+}
+document.querySelector(".js-red-button").onclick = function () {
+    alert("red.");
+}
+document.querySelector(".js-green-button").onclick = function () {
+    alert("green.");
+}
+
+document.querySelector(".js-next-button").onclick = function () {
+    alert("next.");
+}
