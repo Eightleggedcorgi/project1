b92f4b8f118c67b432157b24015de2c09f387aad
Daniel Fordham <dfordham46@gmail.com>
2024-05-07 21:46:10 -0400

    remembered setTimeout exists and super simplified my buttons lighting up and dimming again

--- app.js
+++ app.js
@@ -6,17 +6,29 @@ const greenLight = document.querySelector(".green-button");
 
 document.querySelector(".yellow-button").onclick = function () {
     yellowLight.classList.add("selected");
+    setTimeout(function() {
+        yellowLight.classList.remove("selected");
+    }, 600);
 }
 document.querySelector(".blue-button").onclick = function () {
     blueLight.classList.add("selected");
+    setTimeout(function() {
+        blueLight.classList.remove("selected");
+    }, 600);
 }
 document.querySelector(".red-button").onclick = function () {
     redLight.classList.add("selected");
+    setTimeout(function() {
+        redLight.classList.remove("selected");
+    }, 600);
 }
 document.querySelector(".green-button").onclick = function () {
     greenLight.classList.add("selected");
-}
+    setTimeout(function() {
+        greenLight.classList.remove("selected");
+    }, 600);
 
+}
 document.querySelector(".next-button").onclick = function () {
     alert("next.");
 }
